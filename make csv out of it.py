e2453d191f33cca9c476478495cfc1c2647bd81c
Flying Bear <Galka1999@gmail.com>
2017-04-21 20:32:05 +0300

    Programm for academic writing updated: output is csv: 'lcontext', 'lcontext_highlight', 'n', 'rcontext', 'rcontext_highlight', 'source', 'date'

--- make csv out of it.py
+++ make csv out of it.py
@@ -1,10 +1,14 @@
+
+
+import csv
 import re
-        
+
 def open_text(fname):
     phrases = []
     with open (fname, 'r', encoding = 'utf-8') as f:
         text = f.read()
     return text
+
 
 def find_examples(text):
     raw_examples = re.findall('<p (?:.|\n)*?\'>.*?<.*?\/p>', text)
@@ -17,19 +21,67 @@ def find_examples(text):
         else:
             source = ''
             date = ''
-        n = re.search('(?:Н|н)и шиша', ex).group(0)
+        n_case = re.search('(Н|н)и(?:\n|.)*?шиша', ex)
+        if n_case:
+            n_case = n_case.group(1)
+        else:
+            n_case = ''
+        raw_n = re.search('(Н|н)и(?:\n|.)*?шиша', ex)
+        if raw_n:
+            ex = re.sub('(Н|н)и(?:\n|.)*?шиша', n_case+'и шиша', ex)
+        else:
+            lines_list = re.findall('RU\'>((?:.|\n)*?)<', ex)
+            non_ex_list = []
+            for match in lines_list:
+                print(match)
+        n = re.search('(?:Н|н)и шиша', ex)
         if n:
-            lcontext = re.split('(?:Н|н)и шиша', ex)[0].strip()
-            rcontext = re.split('(?:Н|н)и шиша', ex)[1].strip()
+            n = n.group(0)
+            raw_lcontext = re.split('(?:Н|н)и шиша', ex)[0].strip()
+            raw_rcontext = re.split('(?:Н|н)и шиша', ex)[1].strip()
+            raw_rcontext = re.sub('\[.*?\]','', raw_rcontext)
+        else:
+            raw_lcontext = ''
+            raw_rcontext = ''
+        lcontext = re.findall('RU\'>((?:.|\n)*?)<', raw_lcontext)
+        if lcontext:
+            match_list = []
+            for match in lcontext:
+                match_list.append(match)
+            lcontext = ' '.join(match_list)
         else:
             lcontext = ''
+        rcontext = re.findall('RU\'>((?:.|\n)*?)<', raw_rcontext)
+        if rcontext:
+            match_list = []
+            for match in rcontext:
+                match_list.append(match)
+            rcontext = ' '.join(match_list)
+        else:
             rcontext = ''
-        examples.append([lcontext, n, rcontext, source, date])
-        print()
+        lcontext_highlight = re.search('<i(?:.|\n)*?RU\'>((?:.|\n)*?)<(?:.|\n)*?<\/i>', raw_lcontext)
+        if lcontext_highlight:
+            lcontext_highlight = lcontext_highlight.group(1)
+        else:
+            lcontext_highlight = ''
+        rcontext_highlight = re.search('<i(?:.|\n)*?RU\'>((?:.|\n)*?)<(?:.|\n)*?<\/i>', raw_rcontext)
+        if rcontext_highlight:
+            rcontext_highlight = rcontext_highlight.group(1)
+        else:
+            rcontext_highlight = ''
+        examples.append([lcontext, lcontext_highlight, n, rcontext, rcontext_highlight, source, date])
     return examples
 
+
 def main():
-    print(find_examples(open_text('test.txt'))[0])
+    
+    examples = find_examples(open_text('html.txt'))
+    with open('example_final_spreadsheet.csv', 'w', encoding='utf-8') as n:
+        text = csv.writer(n, delimiter=',')
+        header = ['lcontext', 'lcontext_highlight', 'n', 'rcontext', 'rcontext_highlight', 'source', 'date'] # это будет заголовок
+        text.writerow(header)
+        for row in examples:
+            text.writerow(row)
 
 if __name__ == '__main__':
     main()
